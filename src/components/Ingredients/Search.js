b1e3449eecfc68f1582d0ceff2769c5d2e15f99c
Jesica Martin <jesmrtn11@gmail.com>
2020-07-08 13:44:40 +0200

    Get the current input value with inputRef hook

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,10 +6,13 @@ import './Search.css';
 const Search = React.memo(props => {
   const { onLoadIngredients } = props;
   const [enteredFilter, setEnteredFilter] = useState('');
+  const inputRef = useRef();
 
   useEffect(() => {
 
     setTimeout(() => {
+
+      if (enteredFilter)
       const query = 
       enteredFilter.length === 0 
       ? '' 
@@ -36,7 +39,11 @@ const Search = React.memo(props => {
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" value={enteredFilter} onChange={event => setEnteredFilter(event.target.value)} />
+          <input 
+            ref={inputRef}
+            type="text" 
+            value={enteredFilter} 
+            onChange={event => setEnteredFilter(event.target.value)} />
         </div>
       </Card>
     </section>
